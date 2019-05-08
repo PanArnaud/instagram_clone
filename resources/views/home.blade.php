6d5ba861564dd13a294b31ddf857b42d0edde09f
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2019-05-08 22:32:29 +0400

    Set up home page design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img class="rounded-circle" src="https://instagram.frun3-1.fna.fbcdn.net/vp/be948c2898be3a28d64891ee913a5386/5D7778E0/t51.2885-19/s150x150/56887248_643803179385666_8609170195740622848_n.jpg?_nc_ht=instagram.frun3-1.fna.fbcdn.net" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <h1>dadaxarno</h1>
+            <div class="d-flex">
+                <div class="pr-5"><strong>347</strong> posts</div>
+                <div class="pr-5"><strong>422</strong> followers</div>
+                <div class="pr-5"><strong>369</strong> followings</div>
             </div>
+            <div class="pt-4 font-weight-bold">Dada Arn'Ø</div>
+            <div>FTG EAMARQJLD</div>
+            <div><a href="https://github.com/PanArnaud">PanArnaud</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.frun3-1.fna.fbcdn.net/vp/6e82564a35d331ba03e0d000e819819a/5D521020/t51.2885-15/sh0.08/e35/s640x640/58454152_2623393167730626_2772252567974069011_n.jpg?_nc_ht=instagram.frun3-1.fna.fbcdn.net" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.frun3-1.fna.fbcdn.net/vp/3ccf592bd6c65ec2f048be14bc148b11/5D7052F5/t51.2885-15/sh0.08/e35/s640x640/58409478_136161237535829_6435437054333706752_n.jpg?_nc_ht=instagram.frun3-1.fna.fbcdn.net" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.frun3-1.fna.fbcdn.net/vp/a33a6c042985c2b07f5d116c7c937c06/5D59C41D/t51.2885-15/sh0.08/e35/s640x640/58453878_336020717104115_8493776041430372654_n.jpg?_nc_ht=instagram.frun3-1.fna.fbcdn.net" alt="">
         </div>
     </div>
 </div>
