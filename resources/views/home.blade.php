4c21f1259a842de41c75eef8d3d42de24e520845
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2019-05-14 21:58:53 +0400

    Add a full of stuff to get an user profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,17 @@
             <img class="rounded-circle" src="https://instagram.frun3-1.fna.fbcdn.net/vp/be948c2898be3a28d64891ee913a5386/5D7778E0/t51.2885-19/s150x150/56887248_643803179385666_8609170195740622848_n.jpg?_nc_ht=instagram.frun3-1.fna.fbcdn.net" alt="">
         </div>
         <div class="col-9 pt-5">
-            <h1>dadaxarno</h1>
+            <div>
+                <h1>{{ $user->username }}</h1>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>347</strong> posts</div>
                 <div class="pr-5"><strong>422</strong> followers</div>
                 <div class="pr-5"><strong>369</strong> followings</div>
             </div>
-            <div class="pt-4 font-weight-bold">Dada Arn'Ø</div>
-            <div>FTG EAMARQJLD</div>
-            <div><a href="https://github.com/PanArnaud">PanArnaud</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="https://github.com/PanArnaud">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
